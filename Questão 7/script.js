d6a3aefa6a9930b2549365aed701858aabc0183d
Miqueiasxz <99367419+Miqueiasxz@users.noreply.github.com>
2022-10-14 11:02:11 -0300

    Add files via upload

---
+++ Questão 7/script.js
@@ -0,0 +1,15 @@
+let base = 12;
+let altura = 7;
+
+let area;
+let perimetro;
+
+document.write("O valor da Base é --> ", base +"<br>");
+document.write("O valor da Altura é --> ", altura +"<br>"+"<br>");
+
+area = base * altura;
+document.write("A Area do Retangulo é: ", area +"<br>");
+
+perimetro = 2*base+2*altura;
+document.write("A Area do Perimetro é: ", perimetro);
+
