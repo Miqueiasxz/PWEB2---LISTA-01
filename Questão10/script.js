5c8ed5c46e1251d8641d283a3ccac468affedeb1
Miqueiasxz <99367419+Miqueiasxz@users.noreply.github.com>
2022-10-14 10:57:38 -0300

    Add files via upload

---
+++ Questão10/script.js
@@ -0,0 +1,9 @@
+let numero = parseInt(prompt("Digite um valor"));
+
+if (numero>0){
+    document.write("É POSITIVO"+"<br>");
+}else if(numero<0){
+    document.write("É NEGATIVO"+"<br>");
+}else{
+    document.write("É IGUAL A ZERO"+"<br>")
+}
