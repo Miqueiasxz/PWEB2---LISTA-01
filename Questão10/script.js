7cfd3873e5082028297800607f26d614e5ad2404
Miqueiasxz <99367419+Miqueiasxz@users.noreply.github.com>
2022-10-14 10:57:04 -0300

    Delete Questão10 directory

--- Questão10/script.js
+++
@@ -1,9 +0,0 @@
-let numero = parseInt(prompt("Digite um valor"));
-
-if (numero>0){
-    document.write("É POSITIVO"+"<br>");
-}else if(numero<0){
-    document.write("É NEGATIVO"+"<br>");
-}else{
-    document.write("É IGUAL A ZERO!"+"<br>")
-}
